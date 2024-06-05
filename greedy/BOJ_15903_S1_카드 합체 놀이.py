0bf236252f31abb5ef117601bd7c51d42d576f27
JunSeokCheon <goosw775@naver.com>
2024-06-05 22:27:11 +0900

    upadte

--- greedy/BOJ_15903_S1_카드 합체 놀이.py
+++ greedy/BOJ_15903_S1_카드 합체 놀이.py
@@ -1,3 +1,7 @@
+# 해당 문제는 가장 작은 수 2개를 뽑아서 처리해주는 로직이 필요하다.
+# 가장 작은 수를 찾는 알고리즘이 중요한데, sort를 사용할 시 시간복잡도는 nlogn으로 조건에 초과된다.
+# 최소힙을 사용하여 항상 앞에 있는 수가 최소값을 보장해주는 성질을 이용해서 풀이한다.
+# 핵심 : 최소힙
 import sys
 import heapq
 
@@ -12,5 +16,5 @@ for _ in range(m):
     sum_num = first_num + second_num
     heapq.heappush(n_list, sum_num)
     heapq.heappush(n_list, sum_num)
-    
+
 print(sum(n_list))
