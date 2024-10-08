e16c50aeea16a103ebf9cdffe3585296071c2874
JunSeokCheon <goosw775@naver.com>
2024-10-08 21:27:59 +0900

    UPDATE: 풀이 추가

--- Graph/BOJ_2665_G4_미로만들기.py
+++ Graph/BOJ_2665_G4_미로만들기.py
@@ -1,3 +1,38 @@
+# import sys, heapq
+
+# def dijkstra(x, y):
+#     que = []
+#     heapq.heappush(que, (0, x, y))
+#     distance[x][y] = 0
+    
+#     while que:
+#         dist, a, b = heapq.heappop(que)
+        
+#         if dist > distance[a][b]:
+#             continue
+        
+#         for k in range(4):
+#             nx = a + dx[k]
+#             ny = b + dy[k]
+            
+#             if nx < 0 or nx >= n or ny < 0 or ny >= n:
+#                 continue
+            
+#             if dist + 1 < distance[nx][ny]:
+#                 if board[nx][ny] == 1:
+#                     distance[nx][ny] = min(dist, distance[nx][ny])
+#                     heapq.heappush(que, (dist, nx, ny))
+#                 else:
+#                     distance[nx][ny] = min(dist + 1, distance[nx][ny])
+#                     heapq.heappush(que, (dist+1, nx, ny))
+# dx = [-1, 0, 1, 0]
+# dy = [0, -1, 0, 1]
+# n = int(sys.stdin.readline())
+# board = [list(map(int, sys.stdin.readline().strip())) for _ in range(n)]
+# distance = [[1e9]*(n) for _ in range(n)]
+# dijkstra(0,0)
+# print(distance[n-1][n-1])
+
 import sys, heapq
 
 def dijkstra(x, y):
@@ -33,39 +68,3 @@ board = [list(map(int, sys.stdin.readline().strip())) for _ in range(n)]
 distance = [[0]*(n) for _ in range(n)]
 
 print(dijkstra(0,0))
-
-# import sys, heapq
-
-# def dijkstra(x,y):
-#     que = []
-#     heapq.heappush(que, (0, x, y))
-#     visited[x][y] = 1
-    
-#     while que:
-#         dist, a, b = heapq.heappop(que)
-        
-#         if a == n-1 and b == n-1:
-#             return dist
-        
-#         for k in range(4):
-#             nx = a + dx[k]
-#             ny = b + dy[k]
-            
-#             if nx < 0 or nx >= n or ny < 0 or ny >= n:
-#                 continue
-            
-#             if visited[nx][ny] == 0:
-#                 visited[nx][ny] = 1
-#                 if board[ny][ny] == 0:
-#                     heapq.heappush(que, (dist+1, nx, ny))
-#                 else:
-#                     heapq.heappush(que, (dist, nx, ny))
-
-# dx = [-1, 0, 1, 0]
-# dy = [0, -1, 0, 1]
-
-# n = int(sys.stdin.readline())
-# board = [list(map(int, sys.stdin.readline().strip())) for _ in range(n)]
-# visited = [[0]*n for _ in range(n)]
-
-# print(dijkstra(0,0))
